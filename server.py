c4e716019042986aff83b3e1fb86e25a93f98e93
BrandNew8787 <80979158+BrandNew8787@users.noreply.github.com>
2023-04-12 17:16:11 -0700

    server.py with comments

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+# import necessary modules
 import socket
 from cryptography.fernet import Fernet
 
-
+# generate encryption key and write to file
 key = Fernet.generate_key()
 with open('thekey.key', 'wb') as key_file:
     key_file.write(key)
@@ -9,22 +10,28 @@ with open('key.txt', 'wb') as key_text:
     key_text.write(key)
     print(key)
 
+# set up server socket    
 HOST = socket.gethostname()
 IP_ADDRESS = socket.gethostbyname(HOST)
 PORT = 9090
 
+# write encryption key to file
 with open("thekey.key", "wb") as thekey:
     thekey.write(key)
 
+# create server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((IP_ADDRESS, PORT))
 server_socket.listen(1)
 
+# print server information
 print(f"Server is listening for incoming connections at: {IP_ADDRESS}:{PORT}")
 
+# continuously accept client connections and send encryption key
 while True:
     client_socket, client_address = server_socket.accept()
 
+    # send encryption key
     client_socket.sendall(key)
     client_socket.close()
 
